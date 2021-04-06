50584e55355ed3dca6a878e72d1faa520ff5ece4
Avvessalom <avvessalom@gmail.com>
2021-04-06 18:41:03 +0300

    switch to selenoid. TODO - write howTo

--- src/test/kotlin/com/gmail/eugene/lazurin/jetBrainsInternship/LoginPageTest.kt
+++ src/test/kotlin/com/gmail/eugene/lazurin/jetBrainsInternship/LoginPageTest.kt
@@ -2,24 +2,32 @@ package com.gmail.eugene.lazurin.jetBrainsInternship
 
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
+
+import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import java.util.concurrent.TimeUnit
+import org.openqa.selenium.remote.RemoteWebDriver
+import java.net.URI
+
 
 class LoginPageTest {
-    private lateinit var driver: WebDriver
+    private lateinit var driver: RemoteWebDriver
     private lateinit var loginPage: LoginPage
+    private lateinit var capabilities: DesiredCapabilities
     private val url = "https://teamcity.jetbrains.com/"
 
     @BeforeEach
     fun setUp() {
-        System.setProperty(
-            "webdriver.chrome.driver",
-            "src/main/kotlin/com/gmail/eugene/lazurin/jetBrainsInternship/drivers/chromedriver.exe"
+        capabilities = DesiredCapabilities()
+        capabilities.setCapability("browserName", "chrome")
+        capabilities.setCapability("browserVersion", "89.0")
+        capabilities.setCapability("enableVNC", true)
+
+        driver = RemoteWebDriver(
+            URI.create("http://localhost:4444/wd/hub").toURL(),
+            capabilities
         )
-        driver = ChromeDriver()
         driver.manage().window().maximize()
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
         driver.get(url)
@@ -65,6 +73,6 @@ class LoginPageTest {
     @Test
     fun loginAsBb() {
         loginPage.bitbucketLogin.click()
-        assertEquals("Войдите, чтобы продолжить - Войти с аккаунтом Atlassian", driver.title)
+        assertTrue(driver.title.equals("Log in to continue - Log in with Atlassian account") || driver.title.equals("Войдите, чтобы продолжить - Войти с аккаунтом Atlassian"))
     }
 }
